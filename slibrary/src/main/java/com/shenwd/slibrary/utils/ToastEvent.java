6616c5614c7c48b8544010374e9c1feca333c71c
woshenwd <woshenwd@gmail.com>
2019-08-21 22:11:59 +0800

    add test toast

---
+++ slibrary/src/main/java/com/shenwd/slibrary/utils/ToastEvent.java
@@ -0,0 +1,12 @@
+package com.shenwd.slibrary.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastEvent {
+
+    public static void show(Context context, String str){
+        Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
+    }
+
+}
